faa812891ddfe0998d5e9c879ca533c767f429f8
mis <quynpce180789@fpt.edu.vn>
2025-02-10 14:19:49 +0700

    order manager

--- src/main/java/DAO/orderDAO.java
+++ src/main/java/DAO/orderDAO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Dao;
+package DAO;
 
 /**
  *
  * @author Nguyen Phu Quy CE180789
  */
-public class order {
+public class orderDAO {
     
 }
